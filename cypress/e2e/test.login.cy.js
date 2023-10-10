c708b426ff4333953938791f22358ef57ed6243b
rizwan wasim <rizwanwasim115@gmail.com>
2023-10-10 14:53:45 +0500

    Final

--- cypress/e2e/test.login.cy.js
+++ cypress/e2e/test.login.cy.js
@@ -1,25 +1,5 @@
 
 describe("Login Functionality of Nutrofunnel", () => {
-    it("Login Functionality with valid credentials", () => {
-        const email = "qatester@newrich.com";
-        const password = "24%UxI^y%H!JQ^TLvFlZ6AK%";
-
-        cy.visit("https://staging.nutrafunnels.com/login/");
-
-        // Find and fill the email and password fields
-        cy.get('#user_login').type(email);
-        cy.get('#user_pass').type(password);
-
-        // Click the login button
-        cy.get('#wp-submit').contains("Sign in").click();
-
-        // cy.visit("https://staging.nutrafunnels.com/coaching-list/")
-        // cy.url().should('include', '/coaching-list/')
-        // cy.url().reload();
-        // cy.visit("https://staging.nutrafunnels.com/");
-    });
-    // Add more test cases for invalid login attempts, edge cases, etc. as needed
-
     it("Login Functionality with invalid credentials", () => {
         const email = "qatester@newrich.com";
         const password = "abc";
@@ -39,4 +19,16 @@ describe("Login Functionality of Nutrofunnel", () => {
    
     });
 
+    it("Login Functionality with valid credentials", () => {
+        const email = "qatester@newrich.com";
+        const password = "24%UxI^y%H!JQ^TLvFlZ6AK%";
+
+        cy.visit("https://staging.nutrafunnels.com/login/");
+        // Find and fill the email and password fields
+        cy.get('#user_login').type(email);
+        cy.get('#user_pass').type(password);
+        // Click the login button
+        cy.get('#wp-submit').contains("Sign in").click();
+
+    });
 });
